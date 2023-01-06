d0a84a27f0b08c56bee988ab6e2f23664da77cc3
Luis Mario <itsluismario@gmail.com>
2023-01-05 22:23:16 -0600

    Using states

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -3,20 +3,25 @@ import '../styles/TodoSearch.css';
 import icon_search from '../assets/icon_search_.svg';
 
 function TodoSearch () {
+    const [searchValue, setSearchValue] = React.useState('');
+    
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
-    return(
-        <div className="SearchContainer">
-            <img src={icon_search} alt={icon_search} className="searchIcon"/>
-            <input 
-                className="TodoSearch" 
-                placeholder="Search" 
-                onChange={onSearchValueChange}
-            />
-        </div>        
-    );
+    return [
+    <div className="SearchContainer">
+    <img src={icon_search} alt={icon_search} className="searchIcon"/>
+    <input 
+        className="TodoSearch" 
+        placeholder="Search" 
+        value={searchValue}
+        onChange={onSearchValueChange}
+    />
+    </div>,     
+    <p>{searchValue}</p>   
+    ];
 }
 
 export { TodoSearch };
